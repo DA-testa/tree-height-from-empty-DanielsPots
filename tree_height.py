867fa0afc01f05b947b37698d34e8c67cb127212
DanielsPots <124889596+DanielsPots@users.noreply.github.com>
2023-03-05 22:27:12 +0000

    commit

--- tree_height.py
+++ tree_height.py
@@ -1,51 +1,56 @@
 # python3
 
-import sys
-import threading
 import os
+
+class Node:
+    def __init__(self, index):
+        self.index = index
+        self.children = []
 
 
 def compute_height(n, parents):
-    # An array of nodes with empty children
-    nodes = [[] for _ in range(n)]
-    # Filling the array of nodes with their corresponding children
-    for i in range(n):
-        if parents[i] == -1:
-            root = 1
+    nodes = [Node(i) for i in range(n)]
+    root = None
+    for i, parent_index in enumerate(parents):
+        if parent_index == -1:
+            root = nodes[i]
         else:
-            nodes[parents[i]].append(i)
-    # Recursivley computing the height of the tree
-    def height(node):
-        if not nodes[node]:
+            nodes[parent_index].children.append(nodes[i])
+    
+    if root is None:
+        return 0
+    
+    def dfs(node):
+        if not node.children:
             return 1
-        return 1 + max(height(child) for child in nodes[node])
-    return height(root)
+        return 1 + max(dfs(child) for child in node.children)
+    
+    return dfs(root)
 
 
 def main():
-    # implement input form keyboard and from files
-    file_name = input().strip()
-    # let user input file name to use, don't allow file names with letter 'a'
-    while 'a' in file_name:
-        print("Invalid file, enter a file name without the letter 'a'")
-        file_name = input().strip()
-    # check if the file exists in the folder
-    while not os.path.exists(f"test/{file_name}"):
-        print("File not found, enter a valid file name")
-        file_name = input().strip()
-        while 'a' in file_name:
-            print("Invalid file, enter a file name without the letter 'a'")
-            file_name = input().strip()
-    # read inputs from the file
-    with open(f"test/{file_name}", 'r') as f:
-        n = int(f.readline())
-        parents = list(map(int, f.readline().split()))
-    # call the function and output its result
+    # input from keyboard
+    file_name = input("Enter input file name (press Enter for keyboard input): ")
+    if file_name and 'a' not in file_name:
+        if os.path.exists(f"test/{file_name}"):
+            with open(f"test/{file_name}") as f:
+                n = int(f.readline().strip())
+                parents = list(map(int, f.readline().split()))
+        else:
+            print(f"File 'inputs/{file_name}' not found.")
+            return
+    else:
+        n = int(input("Enter the number of nodes: "))
+        parents = list(map(int, input("Enter the parents of the nodes (space-separated): ").split()))
+    
     print(compute_height(n, parents))
+
 
 # In Python, the default limit on recursion depth is rather low,
 # so raise it here for this problem. Note that to take advantage
 # of bigger stack, we have to launch the computation in a new thread.
+import sys, threading
 sys.setrecursionlimit(10**7)  # max depth of recursion
 threading.stack_size(2**27)   # new thread will get stack of such size
 threading.Thread(target=main).start()
+
