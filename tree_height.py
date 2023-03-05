9849cfdb32237aa459fbe7f84a41500061ece5f4
DanielsPots <124889596+DanielsPots@users.noreply.github.com>
2023-03-05 22:56:59 +0000

    commit

--- tree_height.py
+++ tree_height.py
@@ -1,56 +1,57 @@
 # python3
 
-import os
+import sys
+import threading
+import numpy
 
 class Node:
-    def __init__(self, index):
-        self.index = index
+    def init(self, value):
+        self.value = value
         self.children = []
 
-
-def compute_height(n, parents):
+def tree_build(n, parents):
     nodes = [Node(i) for i in range(n)]
-    root = None
-    for i, parent_index in enumerate(parents):
-        if parent_index == -1:
+    for i in range(n):
+        parent = parents[i]
+        if parent == -1:
             root = nodes[i]
         else:
-            nodes[parent_index].children.append(nodes[i])
-    
-    if root is None:
-        return 0
-    
-    def dfs(node):
-        if not node.children:
-            return 1
-        return 1 + max(dfs(child) for child in node.children)
-    
-    return dfs(root)
+            nodes[parent].children.append(nodes[i])
+    return root
+
+def get_height(root):
+    if not root.children:
+        return 1
+    max_depth = 0
+    for child in root.children:
+        max_depth = max(max_depth, get_height(child))
+    return max_depth+1
+        
+
+def compute_height(n, parents):
+    root = tree_build(n, parents)
+    return get_height(root)
 
 
 def main():
-    # input from keyboard
-    file_name = input("Enter input file name (press Enter for keyboard input): ")
-    if file_name and 'a' not in file_name:
-        if os.path.exists(f"test/{file_name}"):
-            with open(f"test/{file_name}") as f:
-                n = int(f.readline().strip())
-                parents = list(map(int, f.readline().split()))
-        else:
-            print(f"File 'inputs/{file_name}' not found.")
-            return
+    input_type = input().strip()
+    if input_type == 'I':
+        n = int(input().strip())
+        parents = list(map(int, input().strip().split()))
     else:
-        n = int(input("Enter the number of nodes: "))
-        parents = list(map(int, input("Enter the parents of the nodes (space-separated): ").split()))
-    
+        file = input().strip()
+        if 'a' in file:
+            print("Error")
+            return
+        with open (f"test/{file}") as f:
+            n = int(f.readline().strip())
+            parents = list(map(int, f.readline().strip().split()))
     print(compute_height(n, parents))
 
 
 # In Python, the default limit on recursion depth is rather low,
 # so raise it here for this problem. Note that to take advantage
 # of bigger stack, we have to launch the computation in a new thread.
-import sys, threading
 sys.setrecursionlimit(10**7)  # max depth of recursion
 threading.stack_size(2**27)   # new thread will get stack of such size
 threading.Thread(target=main).start()
-
